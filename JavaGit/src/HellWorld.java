2ecd74c2b8a7fc56c43d002a923fc3f9a93c4ff0
TeamJJang <serbible2021@gmail.com>
2021-09-27 04:49:51 +0900

    브랜치 변경사항 커밋

--- JavaGit/src/HellWorld.java
+++ JavaGit/src/HellWorld.java
@@ -4,6 +4,7 @@ public class HellWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Checkout:: Branch");
+		System.out.println("Branch Test Commit!");
 
 	}
 
